add78c85b43d746862aa44ccd5590a03d10a2f97
fraciah <karfraciah@gmail.com>
2023-08-27 19:23:13 +0300

    Added some comments

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,14 @@ function App() {
     const [wonGame, setWonGame] =  useState(false) 
     const [rolls, setRolls] = useState(0);
     const [time, setTime] = useState(0);
+
+    //tracking best time achieved
     const [bestTime, setBestTime] = useState(() => {
-      const savedBestTime = localStorage.getItem('bestTime');
-      if(savedBestTime){
+      const savedBestTime = localStorage.getItem('bestTime');    //retrieve best time from local storage
+      if(savedBestTime){         //if best time exists in local storage,covnert it to a number and use it as initial value for bestTime
         return Number(savedBestTime);
       }
-      return null;
+      return null;           //else use null as the initial value for bestTime
     });
 
     useEffect(() => {
